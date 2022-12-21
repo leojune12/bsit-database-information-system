104b0a9aa95dc4faedce9da58d5faa0182f8d1ab
Leo <leojunebedeo@gmail.com>
2022-12-22 01:49:58 +0700

    Update Services component

--- resources/js/Pages/Home/Components/Services.vue
+++ resources/js/Pages/Home/Components/Services.vue
@@ -17,8 +17,8 @@
                     <div class="tw-flex tw-justify-center tw-items-center tw-mb-4 tw-w-10 tw-h-10 tw-rounded-full tw-bg-blue-100 lg:tw-h-12 lg:tw-w-12 dark:tw-bg-blue-900">
                         <svg class="tw-w-5 tw-h-5 tw-text-blue-600 lg:tw-w-6 lg:tw-h-6 dark:tw-text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
                     </div>
-                    <h3 class="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-                    <p class="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
+                    <h3 class="tw-mb-2 tw-text-xl tw-font-bold dark:tw-text-white">Legal</h3>
+                    <p class="tw-text-gray-500 dark:tw-text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
                 </div>
                 <div>
                     <div class="tw-flex tw-justify-center tw-items-center tw-mb-4 tw-w-10 tw-h-10 tw-rounded-full tw-bg-blue-100 lg:tw-h-12 lg:tw-w-12 dark:tw-bg-blue-900">
