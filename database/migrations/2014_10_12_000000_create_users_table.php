a8111eca3c3faced377c1318d75bc451cb22f51a
Leo <leojunebedeo@gmail.com>
2023-01-11 01:40:02 +0800

    Add soft delete to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -39,6 +39,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->boolean('is_active')->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
