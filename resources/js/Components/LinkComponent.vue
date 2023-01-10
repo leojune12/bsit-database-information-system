a633b53987e7500ceb02ed0d2acdeabac06a89eb
Leo <leojunebedeo@gmail.com>
2023-01-11 01:19:46 +0800

    Update button components

--- resources/js/Components/LinkComponent.vue
+++ resources/js/Components/LinkComponent.vue
@@ -13,8 +13,12 @@
     });
 
     const classes = {
-        primary: 'tw-flex tw-justify-center tw-items-center tw-px-4 tw-h-10 tw-bg-blue-600 tw-border tw-border-transparent tw-rounded-md tw-font-semibold tw-text-xs tw-text-white tw-uppercase tw-tracking-widest hover:tw-bg-blue-700 focus:tw-bg-blue-700 active:tw-bg-blue-700 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150',
+        primary: 'tw-flex tw-justify-center tw-items-center tw-px-4 tw-h-10 tw-bg-pink-700 tw-border tw-border-transparent tw-rounded-md tw-font-semibold tw-text-xs tw-text-white tw-uppercase tw-tracking-widest hover:tw-bg-pink-800 focus:tw-bg-pink-800 active:tw-bg-pink-800 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150',
+
         success: 'tw-flex tw-justify-center tw-items-center tw-px-4 tw-h-10 tw-bg-green-600 tw-border tw-border-transparent tw-rounded-md tw-font-semibold tw-text-xs tw-text-white tw-uppercase tw-tracking-widest hover:tw-bg-green-700 focus:tw-bg-green-700 active:tw-bg-green-700 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-green-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150',
+
+        'success-outlined': 'tw-flex tw-justify-center tw-items-center tw-px-4 tw-h-10 tw-bg-green-600s tw-border-2 tw-border-green-600 tw-rounded-md tw-font-semibold tw-text-xs tw-text-green-600 tw-uppercase tw-tracking-widest hover:tw-bg-green-700 hover:tw-text-white active:tw-text-white focus:tw-text-white focus:tw-bg-green-700 active:tw-bg-green-700 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-green-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150',
+
         secondary: 'tw-flex tw-justify-center tw-items-center tw-px-4 tw-h-10 tw-bg-white tw-border tw-border-gray-300 tw-rounded-md tw-font-semibold tw-text-xs tw-text-gray-700 tw-uppercase tw-tracking-widest tw-shadow-sm hover:tw-bg-gray-50-700 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-500 focus:tw-ring-offset-2 disabled:tw-opacity-25 tw-transition tw-ease-in-out tw-duration-150',
     }
 </script>
--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
     <button
         :type="type"
-        class="tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-bg-blue-700 tw-border tw-border-transparent tw-rounded-md tw-font-semibold tw-text-xs tw-text-white tw-uppercase tw-tracking-widest hover:tw-bg-blue-800 focus:tw-bg-blue-700 active:tw-bg-blue-900 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150"
+        class="tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-bg-pink-700 tw-border tw-border-transparent tw-rounded-md tw-font-semibold tw-text-xs tw-text-white tw-uppercase tw-tracking-widest hover:tw-bg-pink-800 focus:tw-bg-pink-700 active:tw-bg-pink-800 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-500 focus:tw-ring-offset-2 tw-transition tw-ease-in-out tw-duration-150"
     >
         <slot />
     </button>
