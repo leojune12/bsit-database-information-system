eb0b8bcc6c34cbe50d95e4cf6398d519611b99c9
Leo <leojunebedeo@gmail.com>
2023-01-11 03:05:15 +0800

    Add address relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Address\City;
+use App\Models\Address\Region;
+use App\Models\Address\Barangay;
+use App\Models\Address\Province;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
@@ -14,11 +18,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
@@ -26,22 +25,32 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // public function region()
+    // {
+    //     return $this->belongsTo(Region::class, 'region_id', 'regCode');
+    // }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class, 'province_id', 'provCode');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id', 'citymunCode');
+    }
+
+    public function barangay()
+    {
+        return $this->belongsTo(Barangay::class, 'barangay_id', 'brgyCode');
+    }
 }
