7543f9e6e31d2bbf3c9c8716d0257062bb88e471
Leo <leojunebedeo@gmail.com>
2023-01-12 23:37:16 +0800

    Generate production assets

--- public/build/assets/Home-23a76a89.js
+++
@@ -1,1 +0,0 @@
-import o from"./Hero-341eb7e8.js";import{o as r,c as e}from"./app-edaa05cc.js";import"./Login-efccb4a9.js";const s={__name:"Home",setup(t){return(c,_)=>(r(),e(o))}};export{s as default};
