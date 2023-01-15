6b186d116da4836f7ac7e25119bf406646bd9107
Leo <leojunebedeo@gmail.com>
2023-01-15 14:50:50 +0800

    Generate production assets

--- public/build/assets/InputError-b7607a88.js
+++
@@ -1,1 +0,0 @@
-import{h as s,v as t,o as a,g as r,d as o,t as c}from"./app-cb8a8142.js";const n={class:"tw-text-sm tw-text-red-600"},p={__name:"InputError",props:["message"],setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
