33653a866325ce6d9dd00bb0f37acb0a5217e12a
Leo <leojunebedeo@gmail.com>
2023-01-20 16:52:45 +0800

    Add required on input label vue component

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -1,10 +1,18 @@
 <script setup>
-defineProps(['value']);
+defineProps({
+    value: {
+        type: String,
+    },
+    required: {
+        default: false,
+        type: Boolean,
+    }
+});
 </script>
 
 <template>
     <label class="tw-block tw-font-medium tw-text-sm tw-text-gray-700">
-        <span v-if="value">{{ value }}</span>
+        <span v-if="value">{{ value }} <span v-if="required" class="tw-font-black tw-text-red-500">*</span></span>
         <span v-else><slot /></span>
     </label>
 </template>
